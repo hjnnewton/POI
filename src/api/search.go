77d596b4735add0d755d343031689d1af1e2e90c
Jianan HU <jianan.hu.interns@cloudminds.com>
2017-09-29 15:08:12 +0800

    Get the location info by latitude and longitude

--- src/api/search.go
+++ src/api/search.go
@@ -16,6 +16,12 @@ func SearchPOI(location string, ak string, interest string) string{
 		"&location=" + lat + "," + lng +"&radius=2000&output=json&ak=" + ak)
 	js, _ := simplejson.NewJson(body)
 	pois, _ := json.Marshal(js.Get("results"))
+	status,_ := json.Marshal(js.Get("status"))
+	if string(status) == "302"{
+		fmt.Println("超出日调用限额")
+		return "超出限额，查询失败"
+	}
+
 	fmt.Printf("%s周边的%s有：\n %s", location, interest, string(pois))
 	return string(pois)
 }
